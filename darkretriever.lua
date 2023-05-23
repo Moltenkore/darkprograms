72d4844e7f1f906c20e40c76dc99423c4a56b277
OutragedMetro <mark.hilt1@gmail.com>
2023-05-22 20:30:29 -0600

    Update darkretriever.lua

--- darkretriever.lua
+++ darkretriever.lua
@@ -1,5 +1,6 @@
 local Version = 2.12
 local x, y = term.getSize()
+
 if not http then
   print("Herp derp, forgot to enable HTTP?")
   return
@@ -8,12 +9,11 @@ end
 local function getUrlFile(url)
   local response = http.get(url)
   if response then
-    local fileContent = response.readAll()
+    local content = response.readAll()
     response.close()
-    return fileContent
-  else
-    error("Failed to retrieve URL: " .. url)
+    return content
   end
+  return nil
 end
 
 local function writeFile(filename, data)
@@ -22,25 +22,24 @@ local function writeFile(filename, data)
   file.close()
 end
 
-local function clearScreen()
+local function cs()
   term.clear()
   term.setCursorPos(1, 1)
 end
 
-local function setTextColor(textColor, backgroundColor)
+local function tc(tcolor, bcolor)
   if term.isColor() then
-    if textColor then
-      term.setTextColor(colors[textColor])
+    if tcolor then
+      term.setTextColor(colors[tcolor])
     end
-    if backgroundColor then
-      term.setBackgroundColor(colors[backgroundColor])
+    if bcolor then
+      term.setBackgroundColor(colors[bcolor])
     end
   end
 end
 
-local function centeredWrite(text, line)
-  local xPos = math.floor((x - #text) / 2) + 1
-  term.setCursorPos(xPos, line)
+local function writeC(text, line)
+  term.setCursorPos((x / 2) - (#text / 2), line)
   term.write(text)
 end
 
@@ -52,15 +51,15 @@ function term.write(text)
   term.oldWrite(text)
 end
 
-local function printHeader(text)
-  setTextColor("white", "blue")
-  centeredWrite(string.rep("  ", x), 1)
-  centeredWrite(string.rep("  ", x), y)
-  centeredWrite(text, 1)
-  setTextColor("white", "black")
+local function header(text)
+  tc("white", "blue")
+  writeC(string.rep("  ", x), 1)
+  writeC(string.rep("  ", x), y)
+  writeC(text, 1)
+  tc("white", "black")
 end
 
-local function gitUpdate(programName, filename, programVersion)
+local function gitUpdate(ProgramName, Filename, ProgramVersion)
   if http then
     local status, getGit = pcall(http.get, "https://raw.githubusercontent.com/rservices/darkprograms/darkprograms/programVersions")
     if not status then
@@ -68,19 +67,27 @@ local function gitUpdate(programName, filename, programVersion)
       print("Error: " .. getGit)
       return false
     end
-    getGit = getGit.readAll()
-    local newVersion = textutils.unserialize(getGit)
-    if newVersion[programName].Version > programVersion then
-      getGit = http.get(newVersion[programName].GitURL)
-      getGit = getGit.readAll()
-      writeFile(filename, getGit)
-      return true
+    local getGitContent = getGit.readAll()
+    getGit.close()
+    local NVersion = textutils.unserialize(getGitContent)
+    if NVersion[ProgramName].Version > ProgramVersion then
+      getGit = http.get(NVersion[ProgramName].GitURL)
+      if getGit then
+        local getGitContent = getGit.readAll()
+        getGit.close()
+        writeFile(Filename, getGitContent)
+        return true
+      else
+        print("\nFailed to download updated program.")
+        return false
+      end
     end
+  else
+    return false
   end
-  return false
 end
 
-clearScreen()
+cs()
 print("Checking for updates...")
 if gitUpdate("darkretriever", shell.getRunningProgram(), Version) then
   print("Update found and downloaded.")
@@ -92,234 +99,204 @@ end
 sleep(1)
 
 x, y = term.getSize()
-clearScreen()
-write("-> Grabbing file...")
+cs()
+term.write("-> Grabbing file...")
 local cat = getUrlFile("https://raw.githubusercontent.com/rservices/darkprograms/darkprograms/programVersions")
-cat = textutils.unserialize(cat)
-write(" Done.")
-sleep(1)
-clearScreen()
+if cat then
+  cat = textutils.unserialize(cat)
+  term.write(" Done.")
+  sleep(1)
+  cs()
 
-local menu = {}
-local rawName = {}
+  local menu = {}
+  local rawName = {}
 
---[[
+  --[[
+  -Author
+  --Package
+  ---Program
+  ]]--
 
--Author
---Package
----Program
-
-]]--
-
-for name, data in pairs(cat) do
-  if not menu[data.Author] then
-    menu[data.Author] = {}
-  end
-  if not menu[data.Author][data.Package] then
-    menu[data.Author][data.Package] = {}
-  end
-  if not menu[data.Author][data.Package][name] then
-    menu[data.Author][data.Package][data.Name] = data
-    rawName[data.Name] = name
-  end
-end
-
-local state = "top"
-local csel = 1 -- Current selected
-local osel = {1} -- Breadcrumb
-local page = 0
-local ind = 3 -- Y indent
-local ava = y - ind -- Available space
-local level = 1
-
-local function selection(no, list, totpage)
-  term.setCursorPos(1, (no - mod) + (ind - 1))
-  setTextColor("white")
-  term.write("[" .. list[no] .. "]")
-  setTextColor("white", "black")
-  term.setCursorPos(1, y)
-  setTextColor("white", "blue")
-  term.write("Page: " .. page + 1 .. "/" .. totpage)
-  term.setCursorPos(x - 8, y)
-  term.write("By OutragedMetro .INC")
-  setTextColor("white", "black")
-end
-
-local function draw(tbl)
-  local c = 1
-  local sdat = {}
-  local odat = {}
-  for n, d in pairs(tbl) do
-    table.insert(sdat, n)
-    table.insert(odat, d)
-    c = c + 1
-  end
-  if level ~= 4 then
-    table.sort(sdat)
+  for name, data in pairs(cat) do
+    if not menu[data.Author] then
+      menu[data.Author] = {}
+    end
+    if not menu[data.Author][data.Package] then
+      menu[data.Author][data.Package] = {}
+    end
+    if not menu[data.Author][data.Package][name] then
+      menu[data.Author][data.Package][data.Name] = data
+      rawName[data.Name] = name
+    end
   end
 
-  local tpages = math.ceil(c / (y - ind))
-  local mod = page * (y - ind)
+  local state = "top"
+  local csel = 1 --Current selected
+  local osel = {1} --breadcrumb
 
-  for i = 1, y - ind do
-    term.setCursorPos(2, i + ind - 1)
-    term.write(sdat[i + mod])
+  local page = 0
+  local ind = 3 --Y indent
+  local ava = y - ind --Available space
+  local level = 1
+
+  local function selection(no, list, totpage)
+    term.setCursorPos(1, (no - mod) + (ind - 1))
+    tc("white")
+    term.write("[" .. list[no] .. "]")
+    tc("white", "black")
+    term.setCursorPos(1, y)
+    tc("white", "blue")
+    term.write("Page: " .. page + 1 .. "/" .. totpage)
+    term.setCursorPos(x - 8, y)
+    term.write("By OutragedMetro .INC")
+    tc("white", "black")
+  end
+
+  local function draw(tbl)
+    local c = 1
+    local sdat = {}
+    local odat = {}
+    for n, d in pairs(tbl) do
+      table.insert(sdat, n)
+      table.insert(odat, d)
+      c = c + 1
+    end
+    if level ~= 4 then
+      table.sort(sdat)
+    end
+
+    local tpages = math.ceil(c / (y - ind))
+    local mod = page * (y - ind)
+
+    for i = 1, y - ind do
+      term.setCursorPos(2, i + ind - 1)
+      term.write(sdat[i + mod])
+
+      if level == 4 then
+        term.setCursorPos(15, i + ind - 1)
+        if type(odat[i + mod]) == "string" and #odat[i + mod] + 14 > x then
+          term.write(string.sub(odat[i + mod], 1, x - 14 - 2) .. "..")
+        else
+          term.write(odat[i + mod])
+        end
+      end
+    end
 
     if level == 4 then
-      term.setCursorPos(15, i + ind - 1)
-      if type(odat[i + mod]) == "string" and #odat[i + mod] + 14 > x then
-        term.write(string.sub(odat[i + mod], 1, x - 14 - 2) .. "..")
-      else
-        term.write(odat[i + mod])
-      end
+      term.setCursorPos(1, y - 2)
+      writeC("Press enter to download.", y - 2)
     end
+
+    return sdat, tpages, mod
   end
 
-  if level == 4 then
-    term.setCursorPos(1, y - 2)
-    centeredWrite("Press enter to download.", y - 2)
-  end
-
-  return sdat, tpages, mod
-end
-
-local function runMenu()
-  while true do
-    clearScreen()
-    if level == 1 then
-      local list, totpage, mod = draw(menu)
-      printHeader("Authors")
-      setTextColor("white", "black")
-      centeredWrite("Press 'h' for help, 'q' to quit.", 2)
-      setTextColor("white", "black")
-
-    elseif level == 2 then
-      local list, totpage, mod = draw(menu[auna])
-      printHeader("Packages")
-    elseif level == 3 then
-      local list, totpage, mod = draw(menu[auna][pkg])
-      printHeader("Programs")
-    elseif level == 4 then
-      printHeader("Program Data")
-      local list, totpage, mod = draw(menu[auna][pkg][pro])
-    end
-
-    selection(csel, list, totpage)
-
-    local event, key = os.pullEvent("key")
-
-    if key == keys.h then
-      clearScreen()
-      printHeader("Help")
-      term.setCursorPos(1, ind)
-      print("Use the up and down arrows to move through the list.")
-      print("Use the right arrow to enter a menu item and the left arrow to exit.")
-      print("https://outraged-metro.com")
-      setTextColor("white", "black")
-      centeredWrite("Press enter to continue.", y - 2)
-      os.pullEvent("key")
-      level = osel[#osel]
-    end
-
-    if key == keys.q then
-      clearScreen()
-      printHeader("Exit")
-      term.setCursorPos(1, ind)
-      print("Are you sure you want to exit?")
-      print("Press enter to confirm or any other key to cancel.")
-      local event, key = os.pullEvent("key")
-      if key == keys.enter then
-        clearScreen()
-        return
-      else
-        level = osel[#osel]
-      end
-    end
-
-    if key == keys.enter then
+  local function runMenu()
+    while true do
+      cs()
       if level == 1 then
-        auna = list[csel + mod]
-        osel = {csel + mod}
-        level = 2
+        local list, totpage, mod = draw(menu)
+        header("Authors")
+
+        tc("white", "black")
+        writeC("Press 'h' for help, 'q' to quit.", 2)
+        tc("white", "black")
+
       elseif level == 2 then
-        pkg = list[csel + mod]
-        osel[#osel + 1] = csel + mod
-        level = 3
+        local list, totpage, mod = draw(menu[auna])
+        header("Packages")
       elseif level == 3 then
-        pro = rawName[list[csel + mod]]
-        osel[#osel + 1] = csel + mod
-        level = 4
+        local list, totpage, mod = draw(menu[auna][pkg])
+        header("Programs")
       elseif level == 4 then
-        local rname = rawName[list[csel + mod]]
-        clearScreen()
-        printHeader("Download")
+        header("Program Data")
+        local list, totpage, mod = draw(menu[auna][pkg][pro])
+      end
+
+      selection(csel, list, totpage)
+
+      local event, key = os.pullEvent("key")
+
+      if key == keys.h then
+        cs()
+        header("Help")
         term.setCursorPos(1, ind)
-        print("Are you sure you want to download " .. rname .. "?")
-        print("Press enter to confirm or any other key to cancel.")
-        local event, key = os.pullEvent("key")
-        if key == keys.enter then
-          clearScreen()
-          write("-> Grabbing file...")
-          local file = getUrlFile(cat[rname].DownloadURL)
-          writeFile(rname, file)
-          print(" Done.")
-          sleep(1)
-          clearScreen()
-          print("File " .. rname .. " downloaded successfully!")
-          print("Press any key to continue.")
-          os.pullEvent("key")
+        print("Use the up and down arrows to move through the list.")
+        print("Use the right arrow to enter a menu item and the left arrow to exit.")
+        print("https://outragedmetro.wixsite.com/darkprograms\n")
+        print("Press any key to continue.")
+        os.pullEvent("key")
+      elseif key == keys.q then
+        return
+      elseif key == keys.right then
+        if level == 1 then
+          auna = list[csel]
+          level = 2
+          table.insert(osel, csel)
+        elseif level == 2 then
+          pkg = list[csel]
+          level = 3
+          table.insert(osel, csel)
+        elseif level == 3 then
+          pro = rawName[list[csel]]
+          level = 4
+        elseif level == 4 then
+          local meta = menu[auna][pkg][pro]
+          cs()
+          header("Downloading " .. meta.Name)
+          term.setCursorPos(1, ind)
+          term.write("-> Downloading file...")
+
+          local content = getUrlFile(meta.GitURL)
+          if content then
+            local filename = meta.Name .. ".lua"
+            writeFile(filename, content)
+            term.setCursorPos(1, ind + 2)
+            term.write("-> File downloaded successfully!")
+            sleep(2)
+            return
+          else
+            term.setCursorPos(1, ind + 2)
+            term.write("-> Failed to download the file.")
+            sleep(2)
+            return
+          end
         end
-        level = osel[#osel]
-      end
-    end
-
-    if key == keys.right then
-      if level ~= 4 then
-        osel[#osel + 1] = csel + mod
-      end
-      level = level + 1
-    end
-
-    if key == keys.left then
-      if level == 1 then
-        clearScreen()
-        printHeader("Exit")
-        term.setCursorPos(1, ind)
-        print("Are you sure you want to exit?")
-        print("Press enter to confirm or any other key to cancel.")
-        local event, key = os.pullEvent("key")
-        if key == keys.enter then
-          clearScreen()
+        csel = 1
+      elseif key == keys.left then
+        if level == 1 then
           return
+        elseif level == 2 then
+          level = 1
+          table.remove(osel)
+          csel = osel[#osel]
+        elseif level == 3 then
+          level = 2
+          table.remove(osel)
+          csel = osel[#osel]
+        elseif level == 4 then
+          level = 3
         end
-      else
-        level = osel[#osel]
-        table.remove(osel, #osel)
-      end
-    end
-
-    if key == keys.down then
-      if csel < ava then
-        csel = csel + 1
-      else
-        if page + 1 < totpage then
-          page = page + 1
-          csel = 1
+      elseif key == keys.up then
+        if csel > 1 then
+          csel = csel - 1
+        else
+          if page > 0 then
+            page = page - 1
+          end
         end
-      end
-    end
-
-    if key == keys.up then
-      if csel > 1 then
-        csel = csel - 1
-      else
-        if page - 1 >= 0 then
-          page = page - 1
-          csel = ava
+      elseif key == keys.down then
+        if csel < ava then
+          if csel < #list then
+            csel = csel + 1
+          end
+        else
+          if page < totpage - 1 then
+            page = page + 1
+          end
         end
       end
     end
   end
-end
 
-runMenu()
+  runMenu()
+end
