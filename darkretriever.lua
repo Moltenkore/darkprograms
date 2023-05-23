02b9bc7d61c7e8c60e40af782888a4465ac831d2
OutragedMetro <mark.hilt1@gmail.com>
2023-05-22 21:01:07 -0600

    Update darkretriever.lua

--- darkretriever.lua
+++ darkretriever.lua
@@ -1,5 +1,5 @@
 Version = 2.12
-x,y = term.getSize()
+x, y = term.getSize()
 
 if not http then
   print("Herp derp, forget to enable http?")
@@ -20,22 +20,22 @@ end
 
 local function cs()
   term.clear()
-  term.setCursorPos(1,1)
+  term.setCursorPos(1, 1)
 end
 
-local function tc(tcolor,bcolor)
+local function tc(tcolor, bcolor)
   if term.isColor() then
     if tcolor then
       term.setTextColor(colors[tcolor])
     end
     if bcolor then
-      term.setBackgroundColor(colors[bcolor])  
+      term.setBackgroundColor(colors[bcolor])
     end
   end
 end
 
-local function writeC(text,line)
-  term.setCursorPos((x / 2) - (#text / 2),line)
+local function writeC(text, line)
+  term.setCursorPos((x / 2) - (#text / 2), line)
   term.write(text)
 end
 
@@ -48,11 +48,11 @@ function term.write(text)
 end
 
 local function header(text)
-  tc("white","blue")
-  writeC(string.rep("  ",x),1)
-  writeC(string.rep("  ",x),y)
-  writeC(text,1)
-  tc("white","black")
+  tc("white", "blue")
+  writeC(string.rep("  ", x), 1)
+  writeC(string.rep("  ", x), y)
+  writeC(text, 1)
+  tc("white", "black")
 end
 
 local function gitUpdate(ProgramName, Filename, ProgramVersion)
@@ -60,9 +60,9 @@ local function gitUpdate(ProgramName, Filename, ProgramVersion)
     local status, getGit = pcall(http.get, "https://raw.github.com/darkrising/darkprograms/darkprograms/programVersions")
     if not status then
       print("\nFailed to get Program Versions file.")
-      print("Error: ".. getGit)
+      print("Error: " .. getGit)
       return exit
-    end 
+    end
     local getGit = getGit.readAll()
     NVersion = textutils.unserialize(getGit)
     if NVersion[ProgramName].Version > ProgramVersion then
@@ -81,11 +81,11 @@ end
 -- Loading Animation --
 
 -- Define a variable to keep track of the loading duration
-local loadingDuration = 5  -- 5 seconds
+local loadingDuration = 5 -- 5 seconds
 
 -- Modify the loading animation function
 function loadingAnimation()
-  local loadingStartTime = os.clock()  -- Get the starting time
+  local loadingStartTime = os.clock() -- Get the starting time
 
   -- Run the loading animation until the loading duration is reached
   while os.clock() - loadingStartTime < loadingDuration do
@@ -97,7 +97,7 @@ function loadingAnimation()
     -- Add your loading animation logic here
     -- ...
 
-    sleep(0.1)  -- Adjust the delay between frames if needed
+    sleep(0.1) -- Adjust the delay between frames if needed
   end
 end
 
@@ -107,26 +107,16 @@ function runMenu()
   print("Checking for updates...")
   if gitUpdate("darkretriever", shell.getRunningProgram(), Version) == true then
     print("Update found and downloaded.")
-    print("\nPlease run ".. shell.getRunningProgram() .. " again.")
+    print("\nPlease run " .. shell.getRunningProgram() .. " again.")
     return exit
   else
     print("Program up-to-date.")
   end
   sleep(1)
 
-  x,y = term.getSize()
+  x, y = term.getSize()
   cs()
   write("-> Grabbing file...")
-  cat = getUrlFile("https://raw.github.com/darkrising/darkprograms/darkprograms/programVersions")
-  write(" Done.")
-  sleep(1)
-  cs()
-  write("-> Updating file...")
-  writeFile("programVersions",cat)
-  write(" Done.")
-  sleep(1)
-  cs()
-  write("-> Loading file...")
   sleep(1)
   write(" Done.")
   sleep(1)
@@ -137,43 +127,33 @@ function runMenu()
   sleep(1)
   cs()
   write("-> Importing file...")
-  cat = textutils.unserialize(cat)
+  local cat = getUrlFile("https://raw.github.com/darkrising/darkprograms/darkprograms/programList")
   write(" Done.")
   sleep(1)
   cs()
 
-  menu = {}
-  rawName = {}
-  rawCat = {}
-
-  for i = 1, #cat do
-    rawName[i] = cat[i].name
-    rawCat[i] = cat[i].cat
-    if cat[i].hidden ~= true then
-      if not menu[rawCat[i]] then
-        menu[rawCat[i]] = {}
-      end
-      table.insert(menu[rawCat[i]], i)
-    end
-  end
-
-  while true do
+  local function printMenu()
     header("Main Menu")
     local yPrint = 3
-    for i = 1, #rawName do
+    for i = 1, #cat do
       if cat[i].hidden ~= true then
         term.setCursorPos(2, yPrint)
-        print(i .. " - " .. rawName[i])
+        print(i .. " - " .. cat[i].name)
         yPrint = yPrint + 1
       end
     end
+  end
 
+  printMenu()
+
+  while true do
     local event, button, mx, my = os.pullEvent("mouse_click")
-    if mx >= 2 and my >= 3 and mx <= x - 1 and my <= yPrint - 1 then
-      selection = (my - 3) + 1
+    if mx >= 2 and my >= 3 and mx <= x - 1 and my <= y - 1 then
+      local selection = (my - 3) + 1
       shell.run(cat[selection].location)
     end
     cs()
+    printMenu()
   end
 end
 
